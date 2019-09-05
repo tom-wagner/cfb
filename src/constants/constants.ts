83c317da02cbce5439097a5cc8feb939f4d37179
Tom Wagner <tom.wagner@xaxis.com>
2019-09-05 15:05:27 -0700

    Update prod API url

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,4 +1,4 @@
-const PROD_API_URL = "https://mg9vlz351l.execute-api.us-east-1.amazonaws.com/prod";
+const PROD_API_URL = "https://arm1og5wr3.execute-api.us-east-1.amazonaws.com/prod";
 export const API_URL_TO_USE = PROD_API_URL;
 
 // const BASE_API_URL = "http://127.0.0.1:5000";
